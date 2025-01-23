af0812a72db16ba9b5d88c41775f668f3900839d
Lysander <lysander1@schule.bremen.de>
2025-01-23 14:44:33 +0100

    Erstellt ProjektController Klasse

---
+++ src/main/java/de/szut/lf8_starter/ProjectController.java
@@ -0,0 +1,20 @@
+package de.szut.lf8_starter;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/project")
+public class ProjectController {
+
+@GetMapping
+public ResponseEntity<List> getAllProject() {
+
+
+//TODO
+}
+}
