ed481046303af8873746b648bc20e62890eb41f2
Luca Weidler <luca.w12@schule.bremen.de>
2025-01-23 13:33:49 +0100

    Models angelegt

---
+++ src/main/java/de/szut/lf8_starter/model/Employee.java
@@ -0,0 +1,8 @@
+package de.szut.lf8_starter.model;
+
+import java.util.List;
+
+public class Employee {
+    public Employee(int id, String lastName, String firstName, String street,
+                        String postcode, String city, String phone, List<Skill> skillSet ) {}
+}
---
+++ src/main/java/de/szut/lf8_starter/model/Skill.java
@@ -0,0 +1,6 @@
+package de.szut.lf8_starter.model;
+
+public class Skill {
+    public Skill(String skill, int id) {
+    }
+}
