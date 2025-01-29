4f080ff0368e88f87d7a107c0ad60db8b4413e9e
Luca Weidler <luca.w12@schule.bremen.de>
2025-01-29 09:51:43 +0100

    Bissle Lombok angefangen

--- src/main/java/de/szut/lf8_starter/model/Project.java
+++ src/main/java/de/szut/lf8_starter/model/Project.java
@@ -1,5 +1,41 @@
 package de.szut.lf8_starter.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "Projekte")
 public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "project_id")
+    private long Id;
+
+    @Column(name = "project_bezeichnung")
+    private String Bezeichnung;
+
+    @Column(name = "project_verMAid")
+    private int VerMAid;
+
+    @Column(name = "project_kundenID")
+    private int KundenId;
+
+    @Column(name = "project_kommentar")
+    private String Kommentar;
+
+    @Column(name = "project_startDatum")
+    private Date Startdatum;
+
+    @Column(name = "project_geplantesEnddatum")
+    private Date geplantesEnddatum;
+
+    @Column(name = "project_tatsächlichesEnddatum")
+    private Date tatsächlichesEnddatum;
 
 }
