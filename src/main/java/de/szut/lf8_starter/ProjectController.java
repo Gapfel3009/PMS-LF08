45ad30ac0dd12cc632000d31f07fe7659bc8a5d6
Gott <hanpeter2003>
2025-02-11 21:07:46 +0100

    Get Project Get ProjectbyID Get Projects mit Ma Implementiert

--- src/main/java/de/szut/lf8_starter/ProjectController.java
+++ src/main/java/de/szut/lf8_starter/ProjectController.java
@@ -1,9 +1,11 @@
 package de.szut.lf8_starter;
 
+import de.szut.lf8_starter.model.Employee;
+import de.szut.lf8_starter.model.Project;
+import de.szut.lf8_starter.service.ProjectService;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -11,10 +13,54 @@ import java.util.List;
 @RequestMapping("/project")
 public class ProjectController {
 
+
+    private final EnableSpringDataWebSupport.SpringDataWebConfigurationImportSelector springDataWebConfigurationImportSelector;
+    ProjectService projectService;
+
+    public ProjectController(EnableSpringDataWebSupport.SpringDataWebConfigurationImportSelector springDataWebConfigurationImportSelector) {
+        this.springDataWebConfigurationImportSelector = springDataWebConfigurationImportSelector;
+    }
+
+    @PostMapping
+    public void createProject(@RequestBody Project project) {
+        try {
+            this.projectService.createProject(project);
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
 @GetMapping
-public ResponseEntity<List> getAllProject() {
-
-
-//TODO
+public ResponseEntity<List<Project>> getAllProjects() {
+    return projectService.getProjects();
 }
+@DeleteMapping("/{id}")
+    public void deleteProject(@PathVariable int id) {
+    try {
+        projectService.delete(id);
+    }catch (Exception e) {
+        System.out.println(e);
+    }
+}
+@GetMapping("/{id}")
+public ResponseEntity<Project> getProjectDetails(@PathVariable int id) {
+    try{
+        return projectService.getProjectDetails(id);
+    }catch (Exception e) {
+        System.out.println(e);
+    }
+    return null;
+}
+
+@GetMapping("/{id}/employee")
+    public ResponseEntity<List<Employee>> getProjectEmployees(@PathVariable int id) {
+    try{
+        return projectService.getProjectEmployees(id);
+    } catch (Exception e) {
+        System.out.println(e);
+    }
+    return null;
+}
+
 }
--- src/main/java/de/szut/lf8_starter/service/ProjectService.java
+++ src/main/java/de/szut/lf8_starter/service/ProjectService.java
@@ -1,8 +1,47 @@
 package de.szut.lf8_starter.service;
 
+import de.szut.lf8_starter.model.Employee;
+import de.szut.lf8_starter.model.Project;
+import de.szut.lf8_starter.repository.EmployeeRepository;
 import de.szut.lf8_starter.repository.ProjectRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+import java.util.Optional;
 
 public class ProjectService {
     ProjectRepository repository;
+    EmployeeRepository employeeRepository;
 
+public void delete(long id) {
+    if(repository.existsById(id))
+    {
+        repository.deleteById(id);
+    }
+}
+public Project createProject(Project project) {
+    return repository.save(project);
+}
+
+public ResponseEntity<List<Project>> getProjects(){
+    List<Project> projects = repository.findAll();
+    return ResponseEntity.ok(projects);
+}
+
+public ResponseEntity<Project> getProjectDetails(long id) {
+    Optional<Project> project = repository.findById(id);
+    return project.isPresent() ? ResponseEntity.ok(project.get()) : ResponseEntity.notFound().build();
+}
+
+public ResponseEntity<List<Employee>> getProjectEmployees(long id) {
+    return repository.findById(id)
+            .map(project -> {
+                List<Employee> employees = employeeRepository.findAllById(project.getEmployeeId());
+                return ResponseEntity.ok(employees);
+            })
+            .orElse(ResponseEntity.notFound().build());
+}
 }
